636cef93deb913f5b57b8b380aad0885aea7c929
Jackson <jacksontenorio8@gmail.com>
2020-10-09 23:38:39 -0300

    Estação Hack

--- Estação hack_frontend/ex003.js
+++ Estação hack_frontend/ex003.js
@@ -2,5 +2,5 @@ var nome = 'João'
 var idade = 17
 console.log('Olá, João')
 if (idade >= 18) {
-    console.log('Você passou no teste e já pode dirijir!')
+    console.log('Você passou no teste e já pode dirigir!')
 }
