dcad9052c43a7fa7aa4fd281ca023f849bbe13ee
Jackson <jacksontenorio8@gmail.com>
2020-10-10 14:26:19 -0300

    Estação Hack

---
+++ Estação hack_frontend/ex004.js
@@ -0,0 +1,7 @@
+var saldo = 427.09
+if (saldo > 0) {
+    console.log('Seu saldo está positivo! Gostaria de fazer um investimento?')
+}
+if (saldo < 0) {
+    console.log('Seu saldo está negativo! Gostaria de fazer um empréstimo?')
+}
