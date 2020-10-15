f0a4244210280925cad75b6f0041c6e16f491411
Jackson <jacksontenorio8@gmail.com>
2020-10-15 17:25:18 -0300

    Estação Hack

---
+++ Estação hack_frontend/ex018.js
@@ -0,0 +1,6 @@
+var listaDeLucro = [100, 30, 300, -10, 600, 10]
+var lucroTotal = 0
+var numeroDePosicoes = listaDeLucro.length
+for (var i = 0; i < listaDeLucro.length; i++) {
+    lucroTotal = lucroTotal + listaDeLucro[i]
+}
