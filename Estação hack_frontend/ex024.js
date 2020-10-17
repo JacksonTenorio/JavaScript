32c7b4759f0422cde3c4be8fef6388715b32e658
Jackson <jacksontenorio8@gmail.com>
2020-10-17 17:21:12 -0300

    Estação Hack

---
+++ Estação hack_frontend/ex024.js
@@ -0,0 +1,7 @@
+var baralhos = ['Ás', 'Damas', 'Rei', 'Valete']
+for (var i = 0; i < baralhos.length; i++) {
+    if (baralhos[i] == 'Rei') {
+        console.log('Encontrei o Rei!')
+        break
+    }
+}
