d83a2272f12da1d017f506af2aed7e7fa3e910e2
Jackson <jacksontenorio8@gmail.com>
2020-10-18 15:17:16 -0300

    Estação Hack

---
+++ Estação hack_frontend/ex034.js
@@ -0,0 +1,8 @@
+validaCPF('576.524.020-85')
+for (var = 0; i < validaCPF.length; i++) {
+    if (validaCPF[i] == true) {
+        console.log('CPF válido')
+    } else {
+        console.log('CPF inválido')
+    }
+}
